c87e6c77b72f91e07c0f7bc15a7ca87bf3cb46fb
Egor Skorikov <skorikov.egor@yandex.ru>
2020-12-21 13:55:43 +0300

    Hot fix

--- src/cryptLib/cryptManager.h
+++ src/cryptLib/cryptManager.h
@@ -4,11 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "stringUtil/functionChecks.h"
-#include "stringUtil/functionStrip.h"
-#include "dynamicString/core.h"
-#include "stringUtil/functionUpperLower.h"
+
+#include "../stringUtil/functionChecks.h"
+#include "../stringUtil/functionStrip.h"
+#include "../dynamicString/core.h"
+#include "../stringUtil/functionUpperLower.h"
+
 #include "CryptCoder.h"
+
 
 // Обёртка для шифрования/дешифрования по алгоритму цезоря
 void caser_crypt(char *string, char *key, int encode);
